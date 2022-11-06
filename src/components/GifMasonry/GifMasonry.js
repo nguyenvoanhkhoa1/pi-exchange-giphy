9ea4e4fb90a3f0f99fea75c357b1a48c6e172d74
nguyenvoanhkhoa1 <nvoanhkhoa@gmail.com>
2022-11-06 12:07:40 +0700

    complete trending gif masonry table

--- src/components/GifMasonry/GifMasonry.js
+++ src/components/GifMasonry/GifMasonry.js
@@ -1,14 +1,27 @@
 import React from "react";
+import Masonry from "react-masonry-css";
 import clsx from "clsx";
 import { GifItem } from "../../components";
 
 const GifMasonry = (props) => {
   const { items } = props;
+
+  const breakpointColumnsObj = {
+    default: 4,
+    1919: 5,
+    1535: 4,
+    1279: 3,
+    1023: 2,
+  };
+
   return (
-    <div className="columns-2 lg:columns-3 xl:columns-4 xxl:columns-5 gap-3">
-      {console.log(items[5])}
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
       {!!items && items.map((item, index) => <GifItem item={item} />)}
-    </div>
+    </Masonry>
   );
 };
 
